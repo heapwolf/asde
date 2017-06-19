5563b64a5e1681283fb8e42f0dd8dbb5ada4ceca
0x00a <paolo@async.ly>
2017-06-19 11:37:08 +0200

    fixes #1

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 module.exports = (fn, ctx) => (...args) =>
   new Promise((resolve, reject) =>
-    fn.call(ctx, ...args, (err, ...values) => {
-      if (err) return resolve([err])
-      return resolve([null, ...values])
-    })
+    process.nextTick(() =>
+      fn.call(ctx, ...args, (err, ...values) => {
+        if (err) return resolve([err])
+        return resolve([null, ...values])
+      })
+    )
   )
