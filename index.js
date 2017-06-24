163d91f1f6e25a114403b89ab82a2bcf93d92d92
Alvaro <alvaro.bernal.gimeno@gmail.com>
2017-06-24 23:42:39 +0100

    fix error position so it can be ignored

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+
 module.exports = (fn, ctx) => (...args) =>
   new Promise((resolve, reject) =>
     process.nextTick(() =>
       fn.call(ctx, ...args, (err, ...values) => {
-        if (err) return resolve([err])
-        return resolve([null, ...values])
+        if (err) return resolve([null, err])
+        return resolve([...values, null])
       })
     )
   )
